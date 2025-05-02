1badaa3bd42261dcebe3cf5c997119faa7177ee3
Tanimal <bobcheng.bob@gmail.com>
2025-05-02 10:48:21 +0800

    add hw4

---
+++ hw4/mitmproxy/example-mitmproxy.py
@@ -0,0 +1,6 @@
+from mitmproxy import http
+
+def request(flow: http.HTTPFlow) -> None:
+    # This function is called when a request is made to the proxy.
+    # You can modify the request here if needed.
+    pass
